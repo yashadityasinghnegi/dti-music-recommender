906cc24e2968f600e67cc01c3cb8432070af62c4
Haozhi Liao <haozhi.liao@gmail.com>
2023-07-15 23:45:26 -0400

    add genre filter

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def recommend_songs_based_on_lyric(cluster_df, similarities, song_name, count):
-    idx = cluster_df[cluster_df['name']==song_name].index.tolist()[0]
+def recommend_songs_based_on_lyric(filtered_df, similarities, song_name, count):
+    idx = filtered_df[filtered_df['name']==song_name].index.tolist()[0]
 
     sim_scores = list(enumerate(similarities[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
@@ -15,18 +15,25 @@ def recommend_songs_based_on_lyric(cluster_df, similarities, song_name, count):
     song_indcies = [i[0] for i in sim_scores]
     song_recos = []
     for song_index in song_indcies:
-        print(cluster_df.at[song_index, 'name'])
-        song_recos.append(cluster_df.at[song_index, 'name'])
+        print(filtered_df.at[song_index, 'name'])
+        song_recos.append(filtered_df.at[song_index, 'name'])
     return song_recos
 
-def find_cluster_for_song(song_name):
-    idx = df[df['name']==song_name].index.tolist()[0]
-    cluster_df = df[df['cluster'] == df.iloc[idx].cluster].reset_index(drop=True)
-    return cluster_df
+def filter_df_for_song(song_name, genre):
+    row_record = df[df['name']==song_name].to_dict('records')[0]
+    cluster_df = df[df['cluster'] == row_record['cluster']].reset_index(drop=True)
+    filtered_df = cluster_df
+    if genre is not None:
+        mask = filtered_df['Genres'].apply(lambda x: genre in x)
+        filtered_df = filtered_df[mask]
+    #Incase filtered df dones't contains the row any more, add it back
+    if (filtered_df['name']==song_name).any() == False:
+        filtered_df = pd.concat([filtered_df, pd.DataFrame([row_record])], ignore_index=True)
+    return filtered_df.reset_index(drop=True)
 
-def calcualte_similarities(cluster_df):
+def calcualte_similarities(filtered_df):
     cv = CountVectorizer()
-    tsm = cv.fit_transform(cluster_df['clean lyric'])
+    tsm = cv.fit_transform(filtered_df['clean lyric'])
     cosine_sim = cosine_similarity(tsm, tsm)
     return cosine_sim
 
@@ -47,8 +54,11 @@ def webhooks():
 def get_response_for_action(action, parameters):
     if action == 'recommend-similar-songs':
         song_name = parameters['song']
-        cluster_df = find_cluster_for_song(song_name)
-        similiarities = calcualte_similarities(cluster_df)
-        song_recos = recommend_songs_based_on_lyric(cluster_df, similiarities, parameters['song'], 10)
+        genre = None
+        if genre in parameters:
+            genre = parameters['genre']
+        filtered_df = filter_df_for_song(song_name, genre)
+        similiarities = calcualte_similarities(filtered_df)
+        song_recos = recommend_songs_based_on_lyric(filtered_df, similiarities, song_name, 10)
         return 'Here are some songs you may like: ' + ','.join(song_recos)
     return 'Sorry, nothing found'
